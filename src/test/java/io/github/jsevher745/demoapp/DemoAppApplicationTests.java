6be911d6cbc00e85179fb15cd764ff53116e6c96
JSevillanoSalesianos <115581391+JSevillanoSalesianos@users.noreply.github.com>
2024-03-04 10:49:22 +0100

    SubidaPrincipal
    SubidaPrincipal

---
+++ src/test/java/io/github/jsevher745/demoapp/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jsevher745.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
